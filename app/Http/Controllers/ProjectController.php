60ecea75f51ac2c826e45b010d43cf45d8b6fa72
ahof0006 <axel.hoffmann0205@yahoo.com>
2024-05-16 12:40:38 +0200

    begin paginate

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $projects = Project::all();
+        $total = 20;
+
+        $queryParams = $request->query();
+
+        if(is_null($queryParams)){
+            $projects = Project::orderBy("TimeStamp", 'desc')->limit($total)->get();
+        }else{
+            
+        }
 
         return view('list_projects', [
             'projects' => $projects
