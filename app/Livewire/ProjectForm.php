8edde52607eb7992817af3fccb4bf96b442dcd5a
gsto0004 <guillaume.stordeur@ulb.be>
2024-06-07 15:11:12 +0200

    Final form UI

--- app/Livewire/ProjectForm.php
+++ app/Livewire/ProjectForm.php
@@ -15,7 +15,9 @@ use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
@@ -36,111 +38,128 @@ final class ProjectForm extends Component implements HasForms
 
     public function form(Form $form): Form {
         return $form->schema([
-            TextInput::make('Name')
-                ->label('Titre')
-                ->maxLength(255)
-                ->required(),
-            TextInput::make('Organisation')
-                ->label('Organisation')
-                ->maxLength(255)
-                ->required(),
-            Fieldset::make('Deadlines')->
-                schema([
-                    Fieldset::make('1ere deadline')->schema([
-                        DateTimePicker::make('deadline'),
-                        Checkbox::make('Continu')
-                            ->label('Continu')
-                            ->default(False),
-                        Select::make('Justificatif')
-                            ->label('Justificatif')
-                            ->options(['Draft', 'Version finale', 'more...'])
-                        ]),
-                    Fieldset::make('2eme deadline')->schema([
-                        DateTimePicker::make('deadline2'),
-                        Checkbox::make('Continu2')
-                            ->label('Continu')
-                            ->default(False),
-                        Select::make('Justificatif2')
-                            ->label('Justificatif')
-                            ->options(['Draft', 'Version finale', 'more...'])
-                    ]),
-                ]),
-            Select::make('Periodicity')
-                ->label('Periodicité')
-                ->options(['Sans', 'Annuel', 'Tous les deux ans']),
-                DatePicker::make('DateBailleur')
-                    ->label('Date Bailleur'),
-                Checkbox::make('GProj')
-                ->label('Projet majeur')
-                ->default(False),
-                Select::make('InfoType')
-                    ->label("Type d'information")
-                    ->options([
-                        'Financement',
-                        "Séance d'information organisée par l'ULB",
-                        "Séance d'information organisée par un organisme externe"
-                    ]),
-                CheckboxList::make('Types')
-                    ->label('Types de programmes')
-                    ->options(InfoTypes::all()->pluck('Name')->toArray())
-                    ->columns(3),
-                Select::make('Appel')
-                    ->label("Disciplines scientifiques de l'appel")
-                    ->multiple()
-                    ->options(function () {
-                        $categories = ScientificDomainCategory::with('domains')->get();
+            Tabs::make('Tabs')->tabs([
+                Tabs\Tab::make('Informations')->schema([
+                    TextInput::make('Name')
+                        ->label('Titre')
+                        ->maxLength(255)
+                        ->required(),
+                    TextInput::make('Organisation')
+                        ->label('Organisation')
+                        ->maxLength(255)
+                        ->required(),
+                    Checkbox::make('GProj')
+                        ->label('Projet majeur')
+                        ->default(False),
+                    Select::make('InfoType')
+                        ->label("Type d'information")
+                        ->options([
+                            'Financement',
+                            "Séance d'information organisée par l'ULB",
+                            "Séance d'information organisée par un organisme externe"
+                        ])
+                        ->selectablePlaceholder(false),
+                    Select::make('Types')
+                        ->multiple()
+                        ->label('Types de programmes')
+                        ->options(InfoTypes::all()->sortBy('Name')->pluck('Name')->toArray())
+                        ->columns(3),
+                    Select::make('Appel')
+                        ->label("Disciplines scientifiques de l'appel")
+                        ->multiple()
+                        ->options(function () {
+                            $categories = ScientificDomainCategory::with('domains')->get();
 
-                        $options = [];
+                            $options = [];
 
-                        foreach ($categories as $category) {
-                            foreach ($category->domains as $domain) {
-                                $options[$category->title][$domain->id] = $domain->title;
+                            foreach ($categories as $category) {
+                                foreach ($category->domains as $domain) {
+                                    $options[$category->title][$domain->id] = $domain->title;
+                                }
                             }
-                        }
-                        return $options;
-                    }),
-            Textarea::make('ShortDescription')
-                ->label('Description courte')
-                ->maxLength(500)
-                ->hint(fn ($state, $component) => strlen($state) . '/' . $component->getMaxLength())
-                ->live(),
-            MarkdownEditor::make('LongDescription')
-            ->label('Description complète'),
-            MarkdownEditor::make('CriteresAdmission')
-                ->label("Critères d'admission"),
-            MarkdownEditor::make('Financement')
-                ->label("Financement"),
-            MarkdownEditor::make('PourPostuler')
-                ->label("Pour postuler"),
-            Select::make('Geo_zones')
-                ->label("Zones géographiques")
-                ->multiple()
-                ->maxItems(3)
-                ->options(function () {
-                    $options = [
-                        'Monde entier' => 'Monde entier',
-                    ];
-                    $options['Continents'] = Continent::all()->pluck('title', 'id')->toArray();
-                    $options['Pays'] = Pays::all()->pluck('nomPays', )->toArray();
-                    return $options;
-                }),
-            Fieldset::make('Contacts')->schema([
-                Fieldset::make('Internes')->schema([
-                    Repeater::make('Contact')->schema([
-                        TextInput::make('first_name')->label('Prénom'),
-                        TextInput::make('last_name')->label('Nom'),
-                        TextInput::make('email')->label('E-mail')->email(),
-                        TextInput::make('tel')->label('Numéro de téléphone')->tel(),
-                    ])
+                            return $options;
+                        }),
+                    Select::make('Geo_zones')
+                        ->label("Zones géographiques")
+                        ->multiple()
+                        ->maxItems(3)
+                        ->options(function () {
+                            $options = [
+                                'Monde entier' => 'Monde entier',
+                            ];
+                            $options['Continents'] = Continent::all()->pluck('title', 'id')->toArray();
+                            $options['Pays'] = Pays::all()->pluck('nomPays', )->toArray();
+                            return $options;
+                        }),
                 ]),
-                Fieldset::make('Externes')->schema([
-                    Repeater::make('Contact')->schema([
-                        TextInput::make('first_name')->label('Prénom'),
-                        TextInput::make('last_name')->label('Nom'),
-                        TextInput::make('email')->label('E-mail')->email(),
-                        TextInput::make('tel')->label('Numéro de téléphone')->tel(),
-                    ])
+                Tabs\Tab::make('Dates importantes')->schema([
+                    Section::make('Deadlines')->
+                    schema([
+                        Fieldset::make('1ere deadline')->schema([
+                            DateTimePicker::make('deadline'),
+                            Select::make('Justificatif')
+                                ->label('Justificatif')
+                                ->options(['Draft', 'Version finale', 'more...']),
+                            Checkbox::make('Continu')
+                                ->label('Continu')
+                                ->default(False)
+                        ]),
+                        Fieldset::make('2eme deadline')->schema([
+                            DateTimePicker::make('deadline2'),
+                            Select::make('Justificatif2')
+                                ->label('Justificatif')
+                                ->options(['Draft', 'Version finale', 'more...']),
+                            Checkbox::make('Continu2')
+                                ->label('Continu')
+                                ->default(False)
+                                ->inline(true)
+                        ]),
+                    ]),
+                    Select::make('Periodicity')
+                        ->label('Periodicité')
+                        ->options(['Sans', 'Annuel', 'Tous les deux ans'])
+                        ->selectablePlaceholder(false),
+                    DatePicker::make('DateBailleur')
+                        ->label('Date Bailleur'),
                 ]),
+                Tabs\Tab::make('Description')->schema([
+                    Textarea::make('ShortDescription')
+                        ->label('Description courte')
+                        ->maxLength(500)
+                        ->hint(fn ($state, $component) => strlen($state) . '/' . $component->getMaxLength())
+                        ->live(),
+                    MarkdownEditor::make('LongDescription')
+                        ->label('Description complète'),
+                    MarkdownEditor::make('Financement')
+                        ->label("Financement"),
+                ]),
+                Tabs\Tab::make("Critères d'admission")->schema([
+                    MarkdownEditor::make('CriteresAdmission')
+                        ->label(""),
+                ]),
+                Tabs\Tab::make("Pour postuler")->schema([
+                    MarkdownEditor::make('PourPostuler')
+                        ->label(""),
+                ]),
+                Tabs\Tab::make("Contacts")->schema([
+                    Fieldset::make('Internes')->schema([
+                        Repeater::make('contacts_internes')->schema([
+                            TextInput::make('first_name')->label('Prénom'),
+                            TextInput::make('last_name')->label('Nom'),
+                            TextInput::make('email')->label('E-mail')->email(),
+                            TextInput::make('tel')->label('Numéro de téléphone')->tel(),
+                        ])->columns(2)
+                    ]),
+                    Fieldset::make('Externes')->schema([
+                        Repeater::make('contacts_externes')->schema([
+                            TextInput::make('first_name')->label('Prénom'),
+                            TextInput::make('last_name')->label('Nom'),
+                            TextInput::make('email')->label('E-mail')->email(),
+                            TextInput::make('tel')->label('Numéro de téléphone')->tel(),
+                        ])->columns(2)->addActionLabel('+ Nouveau contact')
+                    ]),
+                ])
+
             ]),
         ])->statePath(path: 'data');
     }
