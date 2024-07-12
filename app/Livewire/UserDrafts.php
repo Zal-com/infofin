8ce896869edee542f4a4fc46824fbbe46558567e
gsto0004 <guillaume.stordeur@ulb.be>
2024-07-12 13:19:46 +0200

    users drafts ok

--- app/Livewire/UserDrafts.php
+++ app/Livewire/UserDrafts.php
@@ -3,13 +3,21 @@
 namespace App\Livewire;
 
 use App\Models\Draft;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Actions\Concerns\InteractsWithRecords;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
-class UserDrafts extends Component
+class UserDrafts extends Component implements HasTable, HasForms
 {
+    use InteractsWithTable;
+    use InteractsWithForms;
+
     public function table(Table $table): Table
     {
         return $table->query(Draft::where('poster_id', Auth::id()))
