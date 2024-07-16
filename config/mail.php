e05f68ff354ed039ba48f70f762540b3c1021ef1
gsto0004 <guillaume.stordeur@ulb.be>
2024-07-16 10:10:14 +0200

    test mailing

--- config/mail.php
+++ config/mail.php
@@ -109,8 +109,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'infofin@ulb.be'),
+        'name' => env('MAIL_FROM_NAME', 'Infofin'),
     ],
 
 ];
