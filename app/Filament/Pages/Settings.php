d9de84e46349f1ff8b478928d98cc511b240ffeb
ahof0006 <axel.hoffmann@ulb.be>
2024-07-23 12:29:36 +0200

    Settings for the test

---
+++ app/Filament/Pages/Settings.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Filament\Pages;
+
+class Settings extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.settings';
+}
