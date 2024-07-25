da1a44e500bdcb810a4033ac25aadc48b04ad241
gsto0004 <guillaume.stordeur@ulb.be>
2024-07-25 14:15:16 +0200

    Exceptions panel fonctionnel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,5 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->report(function (Throwable $e) {
+            FilamentExceptions::report($e);
+        });
     })->create();
