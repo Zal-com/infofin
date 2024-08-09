6c2f929896a818763d3fbc4701e3aba543a6c77e
ahof0006 <axel.hoffmann@ulb.be>
2024-08-09 09:05:39 +0200

    added edit button on each project

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,13 +1,20 @@
 @extends('layout')
 @section('content')
     @livewireStyles
-    {{\Diglactic\Breadcrumbs\Breadcrumbs::render('project', $project)}}
-
+    <div class="flex justify-between items-center mb-4">
+        {{ \Diglactic\Breadcrumbs\Breadcrumbs::render('project', $project) }}
+        @can('create', App\Models\Project::class)
+            <a href="{{ url(route('projects.edit', $project->id)) }}"
+               class="inline-flex items-center px-4 py-2 bg-orange-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-500 focus:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                <i class="fa fa-solid fa-plus pr-2"></i>
+                Edit
+            </a>
+        @endcan
+    </div>
     <x-project-show-info :project="$project"/>
     <x-filament::section.description class="pl-5">
-        Dernière modification le {{\Carbon\Carbon::make($project->updated_at)->format('d/m/Y')}}
-        par {{$project->poster->full_name()}}
+        Dernière modification le {{ \Carbon\Carbon::make($project->updated_at)->format('d/m/Y') }}
+        par {{ $project->poster->full_name() }}
     </x-filament::section.description>
     @livewireScripts
 @endsection
-
