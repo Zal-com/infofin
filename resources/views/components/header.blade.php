030f380bb1f974e84169d642b6b046352c3cee79
ahof0006 <axel.hoffmann@ulb.be>
2024-08-09 11:13:00 +0200

    archives only on footer

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,19 +15,8 @@
     </a>
     <nav>
         <div class="container m-auto flex justify-between items-center">
-                <div>    
-                @if(\Illuminate\Support\Facades\Route::currentRouteName() == 'projects.index')
-                    <a href="{{route('projects.archive')}}"
-                    class="inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md
-                    font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-gray-700
-                    active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
-                    transition ease-in-out duration-150"
-                    >
-                        <i class="fa-solid fa-archive pr-2"></i>
-                        Archives
-                    </a>
-                @endif
-                </div>
+            <div>
+            </div>
             <div class="flex gap-2">
                 @auth()
                     <a href="{{route('profile.show')}}"
@@ -36,8 +25,8 @@
                        active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
                        transition ease-in-out duration-150"
                     >
-                    <i class="fa-solid fa-user pr-2"></i>
-                    Profil
+                        <i class="fa-solid fa-user pr-2"></i>
+                        Profil
                     </a>
                     <form method="post" action="{{route('logout')}}">
                         @csrf
