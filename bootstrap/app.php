453f5c6da6c64f38f2cb941ed148bd2488fa8e05
ahof0006 <axel.hoffmann0205@yahoo.com>
2024-08-11 15:44:53 +0200

    trust proxy maybe

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->use([
             RouteStatisticsMiddleware::class,
-
+            \App\Http\Middleware\TrustProxies::class,
         ]);
     })
     ->withSchedule(function (Schedule $schedule) {
