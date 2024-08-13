64a40128a35b58094af3ad759df14ab738f7cc69
gsto0004 <guillaume.stordeur@ulb.be>
2024-08-13 10:43:30 +0200

    text fixes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -14,7 +14,7 @@
     </div>
     <footer class="bg-blue-950 text-white flex sm:flex-row flex-col p-5 justify-between gap-5">
         <div class="contain-content">
-            <img src={{secure_asset('img/ULB_logo_mono_blanc.png')}}>
+            <img src={{asset('img/ULB_logo_mono_blanc.png')}}>
         </div>
         <div class="flex flex-row gap-5">
             <div class="flex justify-center align-middle text-center">
