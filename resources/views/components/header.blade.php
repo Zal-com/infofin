4b9ad7c72da87d6c29388f9e857578db6e66443f
gsto0004 <guillaume.stordeur@ulb.be>
2024-08-13 10:09:22 +0200

    revert issues

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,13 +3,13 @@
         <div class="container flex h-[120px] p-4 w-[75%] m-auto">
             <div class="flex items-center">
                 <img class="max-h-[100%] object-contain pr-4 border-r-2 border-blue-900"
-                     src="{{secure_asset('storage/img/ulb_logo.png')}}" alt="Logo de l'ULB"/>
+                     src="{{asset('storage/img/ulb_logo.png')}}" alt="Logo de l'ULB"/>
             </div>
             <div class="text-blue-900 p-4 m-auto">
                 <h1 class="select-none font-bold text-5xl items-center">Infofin</h1>
             </div>
             <div class="flex-1">
-                <img class="max-h-[100%] float-end" src="{{secure_asset('img/header_img.png')}}"
+                <img class="max-h-[100%] float-end" src="{{asset('img/header_img.png')}}"
                      alt="Image décorative"/>
             </div>
         </div>
