db2858afad036562774e68e1ac0787757c4763d7
gsto0004 <guillaume.stordeur@ulb.be>
2024-08-26 14:37:26 +0200

    colors fixed

--- resources/views/unsubscribe.blade.php
+++ resources/views/unsubscribe.blade.php
@@ -1,9 +1,9 @@
 @extends('layout')
 
 @section('content')
-    <x-filament::section class="container m-auto bg-{{$color}}-900">
+    <x-filament::section class="container m-auto bg-{{$color}}-50">
         <div class="flex flex-col items-center justify-center">
-            <div class="text-6xl text-blue-500 mb-4">
+            <div class="text-6xl text-{{$color}}-600 mb-4">
                 <x-filament::icon icon="{{$icon}}" style="height: 64px; width: 64px;"/>
             </div>
             <div class="text-center text-lg font-semibold text-gray-700">
