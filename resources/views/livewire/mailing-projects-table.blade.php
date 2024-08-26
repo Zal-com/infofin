74560836557cdd48925671babc46249a14303117
gsto0004 <guillaume.stordeur@ulb.be>
2024-08-26 13:48:47 +0200

    removed useless classes

--- resources/views/livewire/mailing-projects-table.blade.php
+++ resources/views/livewire/mailing-projects-table.blade.php
@@ -6,8 +6,7 @@
     </div>
 
     <!-- Information Box -->
-    <x-filament::section style="min-width: 200px; height: fit-content"
-                         class="justify-end bg-white rounded-lg">
+    <x-filament::section style="min-width: 200px; height: fit-content">
         <x-filament::section.heading class="text-lg font-semibold mb-2">Prochain mail</x-filament::section.heading>
         <div class="flex gap-2">
             <x-filament::icon icon="heroicon-o-calendar" style="max-height: 24px; max-width: 24px;"/>
