3fc5d9ae4be4a52a788f172b3d4d7d352ee92bd1
ahof0006 <axel.hoffmann@ulb.be>
2024-08-26 15:35:15 +0200

    subscription add admin

--- app/Filament/Resources/SubscriptionsResource/Pages/CreateSubscriptions.php
+++ app/Filament/Resources/SubscriptionsResource/Pages/CreateSubscriptions.php
@@ -3,11 +3,38 @@
 namespace App\Filament\Resources\SubscriptionsResource\Pages;
 
 use App\Filament\Resources\SubscriptionsResource;
-use Filament\Actions;
+use App\Models\User;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateSubscriptions extends CreateRecord
 {
     protected static string $resource = SubscriptionsResource::class;
     protected static ?string $title = "Nouvelle souscription à la newsletter";
+
+    protected array $scientific_domains = [];
+    protected array $info_types = [];
+
+    protected function beforeValidate()
+    {
+        $this->scientific_domains = $this->data['scientific_domains'] ?? [];
+        $this->info_types = $this->data['info_types'] ?? [];
+    }
+
+    protected function handleRecordCreation(array $data): \Illuminate\Database\Eloquent\Model
+    {
+        $user = User::where("id", $data['email'] + 1)->first();
+
+        if (!empty($this->info_types)) {
+            $user->info_types()->sync($this->info_types);
+        }
+
+        if (isset($this->scientific_domains)) {
+            $user->scientific_domains()->sync($this->scientific_domains);
+        }
+
+        $user->is_email_subscriber = 1;
+        $user->save();
+
+        return $user;
+    }
 }
