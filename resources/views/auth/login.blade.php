1e28f60a7af328ba692fbebf0612a5b7368db788
gsto0004 <guillaume.stordeur@ulb.be>
2024-09-05 14:33:24 +0200

    Edited password reset page + replaced default input components for filament inputs components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,30 +10,33 @@
             <!-- Email Address -->
             <div class="mt-10 w-full">
                 <x-input-label for="email" :value="__('Email')"/>
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                              required
-                              autofocus autocomplete="username"/>
+                <x-filament::input.wrapper>
+                    <x-filament::input id="email" class="block mt-1 w-full" type="email" name="email"
+                                       :value="old('email')"
+                                       required
+                                       autofocus autocomplete="username"/>
+                </x-filament::input.wrapper>
                 <x-input-error :messages="$errors->get('email')" class="mt-2"/>
             </div>
 
             <!-- Password -->
             <div class="mt-4 w-full">
                 <x-input-label for="password" :value="__('Password')"/>
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                              type="password"
-                              name="password"
-                              required autocomplete="current-password"/>
-
+                <x-filament::input.wrapper>
+                    <x-filament::input id="password" class="block mt-1 w-full"
+                                       type="password"
+                                       name="password"
+                                       required autocomplete="current-password"/>
+                </x-filament::input.wrapper>
                 <x-input-error :messages="$errors->get('password')" class="mt-2"/>
             </div>
 
             <!-- Remember Me -->
             <div class="flex mt-4 w-full justify-between">
                 <label for="remember_me" class="pl-2 inline-flex items-center">
-                    <input id="remember_me" type="checkbox"
-                           class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
-                           name="remember">
+                    <x-filament::input.checkbox id="remember_me"
+                                                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                                                name="remember"/>
                     <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
                 @if (Route::has('password.request'))
