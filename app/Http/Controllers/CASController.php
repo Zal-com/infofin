2fad6238f612fd350a44f2ab79f25905ed48780f
ahof0006 <axel.hoffmann0205@yahoo.com>
2024-09-10 18:29:56 +0200

    added some comment for cas tomorrow

--- app/Http/Controllers/CASController.php
+++ app/Http/Controllers/CASController.php
@@ -21,10 +21,28 @@ class CASController extends Controller
          * Authentification CAS :
          * Si l'utilisateur existe dans la base de données avec son matricule, il sera automatiquement connecté avec ses informations
          * Si l'utilisateur n'existe pas dans la base de données, il sera connecté avec son matricule, via le CAS
+         * array:17 [▼ // app/Http/Controllers/CASController.php:27
+         *  "clientIpAddress" => "10.88.2.205"
+         *  "isFromNewLogin" => "true"
+         *  "mail" => "axel.hoffmann@ulb.be"
+         *  "authenticationDate" => "2024-09-10T16:27:41.246108Z"
+         *  "eduPersonAffiliation" => "employee"
+         *  "givenName" => "Axel"
+         *  "successfulAuthenticationHandlers" => "LdapAuthenticationHandler"
+         *  "userAgent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0"
+         *  "ulbEmployeeNumber" => "50023185"
+         *  "credentialType" => "UsernamePasswordCredential"
+         *  "samlAuthenticationStatementAuthMethod" => "urn:oasis:names:tc:SAML:1.0:am:password"
+         *  "uid" => "ahof0006"
+         *  "eduPersonOrgUnitDN" => "ou=dpt rech stari,ou=departement recherche,ou=administration generale,ou=universite libre de bruxelles,ou=orgs,dc=ulb,dc=be"
+         *  "authenticationMethod" => "LdapAuthenticationHandler"
+         *  "serverIpAddress" => "172.31.8.125"
+         *  "longTermAuthenticationRequestTokenUsed" => "false"
+         *  "sn" => "Hoffmann"
+         *  ]
          */
         if (Cas::isAuthenticated()) {
             $attributes = Cas::getAttributes();
-            dd($attributes);
             $matricule = null;
             if (isset($attributes['ulbEmployeeNumber'])) {
                 $matricule = Cas::getAttribute('ulbEmployeeNumber');
