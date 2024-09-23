160cb68b34a71ed609d7a8462e366c5dcb4fabff
ahof0006 <axel.hoffmann@ulb.be>
2024-09-23 14:44:01 +0200

    fix : change class variation (tu me frustres)

--- database/migrations/2024_09_23_143152_projects_collections.php
+++ database/migrations/2024_09_23_143152_projects_collections.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsCollectionsTable extends Migration
-{
+return new class extends Migration {
     /**
      * Exécute les migrations.
      */
@@ -31,4 +30,4 @@ class CreateProjectsCollectionsTable extends Migration
     {
         Schema::dropIfExists('projects_collections');
     }
-}
+};
