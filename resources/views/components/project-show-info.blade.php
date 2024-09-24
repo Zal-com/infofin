54fa911bbe2a34e66ec30e98c5d95da5b2b01b24
ahof0006 <axel.hoffmann@ulb.be>
2024-09-24 14:14:16 +0200

    fix(projet-show):cassure email

--- resources/views/components/project-show-info.blade.php
+++ resources/views/components/project-show-info.blade.php
@@ -255,13 +255,15 @@
                             </p>
                         </x-filament::section.heading>
                         @if(!empty($contact_ulb['email']))
-                            <div class="flex items-center">
-                                <x-filament::icon icon="heroicon-s-at-symbol" class="h-[24px] w-[24px] mr-2"/>
-                                <p class="flex-1 flex-wrap overflow-ellipsis line-clamp-1">
-                                    <a href="mailto:{{trim($contact_ulb['email'])}}">
+                            <div class="flex flex-col sm:flex-row items-start gap-2">
+                                <div class="flex-shrink-0">
+                                    <x-filament::icon icon="heroicon-s-at-symbol" class="h-[24px] w-[24px]"/>
+                                </div>
+                                <div class="flex-grow min-w-0">
+                                    <a href="mailto:{{trim($contact_ulb['email'])}}" class="inline-block break-all">
                                         {{$contact_ulb['email']}}
                                     </a>
-                                </p>
+                                </div>
                             </div>
                         @endif
                     </div>
@@ -281,13 +283,18 @@
                             </p>
                         </x-filament::section.heading>
                         @if($contact_ext['email'] != "")
-                            <div class="flex items-center">
-                                <x-filament::icon icon="heroicon-s-at-symbol" class="h-[24px] w-[24px] mr-2"/>
-                                <p class="flex-1 flex-wrap overflow-ellipsis line-clamp-1">
-                                    <a href="mailto:{{trim($contact_ext['email'])}}">
-                                        {{$contact_ext['email']}}
-                                    </a>
-                                </p>
+                            <div class="flex flex-col sm:flex-row items-start gap-2">
+                                <div class="flex-shrink-0">
+                                    <x-filament::icon icon="heroicon-s-at-symbol" class="h-[24px] w-[24px]"/>
+                                </div>
+                                <div class="flex-grow min-w-0">
+                                    <x-filament::icon icon="heroicon-s-at-symbol" class="h-[24px] w-[24px] mr-2"/>
+                                    <p class="flex-1 flex-wrap overflow-ellipsis line-clamp-1">
+                                        <a href="mailto:{{trim($contact_ext['email'])}}" class="inline-block break-all">
+                                            {{$contact_ext['email']}}
+                                        </a>
+                                    </p>
+                                </div>
                             </div>
                         @endif
                     </div>
