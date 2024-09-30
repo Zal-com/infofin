7fa3469117d03b4373267b577bfb4e2f0c4c1f68
ahof0006 <axel.hoffmann@ulb.be>
2024-09-30 15:30:52 +0200

    feat(collection): ovverrride method

--- app/Filament/Resources/CollectionResource/Pages/CreateCollection.php
+++ app/Filament/Resources/CollectionResource/Pages/CreateCollection.php
@@ -3,10 +3,17 @@
 namespace App\Filament\Resources\CollectionResource\Pages;
 
 use App\Filament\Resources\CollectionResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCollection extends CreateRecord
 {
     protected static string $resource = CollectionResource::class;
+
+    protected function handleRecordCreation(array $data): \Illuminate\Database\Eloquent\Model
+    {
+        $data['id'] = \Illuminate\Support\Str::uuid();
+        $data["user_id"] = auth()->id();
+        dd($data);
+        return static::getModel()::create($data);
+    }
 }
