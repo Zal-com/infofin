12f8a4626094acd8cfc6be3a5b962824a61eb0eb
CePseudoBE <axel.hoffmann0205@yahoo.com>
2024-10-02 15:12:14 +0200

    fix(project/form): fix the fact that we can only but one thing in contact or one space

--- app/Livewire/ProjectEditForm.php
+++ app/Livewire/ProjectEditForm.php
@@ -305,17 +305,17 @@ class ProjectEditForm extends Component implements HasForms
                 Tabs\Tab::make("Contacts")->schema([
                     Fieldset::make('Internes')->schema([
                         Repeater::make('contact_ulb')->schema([
-                            TextInput::make('first_name')->label('Prénom'),
-                            TextInput::make('last_name')->label('Nom'),
-                            TextInput::make('email')->label('E-mail')->email(),
-                        ])->columns(2)->addActionLabel('+ Nouveau contact')->label('')
+                            TextInput::make('first_name')->label('Prénom')->required()->minLength(3),
+                            TextInput::make('last_name')->label('Nom')->required()->minLength(3),
+                            TextInput::make('email')->label('E-mail')->email()->required()->minLength(5),
+                        ])->columns(2)->addActionLabel('+ Nouveau contact')->label(false)->maxItems(3)
                     ]),
                     Fieldset::make('Externes')->schema([
                         Repeater::make('contact_ext')->schema([
-                            TextInput::make('first_name')->label('Prénom'),
-                            TextInput::make('last_name')->label('Nom'),
-                            TextInput::make('email')->label('E-mail')->email(),
-                        ])->columns(2)->addActionLabel('+ Nouveau contact')
+                            TextInput::make('first_name')->label('Prénom')->required()->minLength(3),
+                            TextInput::make('last_name')->label('Nom')->required()->minLength(3),
+                            TextInput::make('email')->label('E-mail')->email()->required()->minLength(5),
+                        ])->columns(2)->addActionLabel('+ Nouveau contact')->label(false)->maxItems(3)
                     ]),
                 ]),
                 Tabs\Tab::make('Documents')->schema([
@@ -429,50 +429,82 @@ class ProjectEditForm extends Component implements HasForms
             'geo_zones' => 'array',
             'deadlines' => 'array',
             'short_description' => 'nullable|string|max:500',
-            'long_description' => 'nullable|array',
-            'funding' => 'nullable|array',
-            'admission_requirements' => 'nullable|array',
-            'apply_instructions' => 'nullable|array',
+            'long_description' => 'array',
+            'funding' => 'array|nullable',
+            'admission_requirements' => 'array|nullable',
+            'apply_instructions' => 'array|nullable',
             'contact_ulb' => 'array',
             'contact_ulb.*.first_name' => 'nullable|string',
             'contact_ulb.*.last_name' => 'nullable|string',
             'contact_ulb.*.email' => 'nullable|email',
             'contact_ext' => 'array',
-            'contact_ext.*.first_name' => 'nullable|string|max:50',
-            'contact_ext.*.last_name' => 'nullable|string|max:50',
-            'contact_ext.*.email' => 'nullable|email|max:255',
+            'contact_ext.*.first_name' => 'string|max:50',
+            'contact_ext.*.last_name' => 'string|max:50',
+            'contact_ext.*.email' => 'email|max:255',
             'status' => 'integer',
             'is_draft' => 'boolean',
             'info_sessions' => 'nullable|array'
         ];
 
-        $validator = Validator::make($this->data, $rules, [], [
+        $messages = [
+            'title.required' => 'Le titre est requis.',
+            'title.string' => 'Le titre doit être une chaîne de caractères.',
+            'title.max' => 'Le titre ne peut pas dépasser :max caractères.',
+            'is_big.boolean' => 'Le champ "Projet Majeur" doit être vrai ou faux.',
+            'organisation_id.required' => 'Le champ Organisation est requis.',
+            'organisation_id.exists' => 'L\'organisation sélectionnée n\'existe pas.',
+            'info_types.array' => 'Les types de programme doivent être remplis.',
+            'documents.array' => 'Les documents doivent être remplis.',
+            'scientific_domains.array' => 'Les disciplines scientifiques doivent être remplies.',
+            'scientific_domains.required' => 'Veuillez sélectionner au moins une discipline scientifique.',
+            'scientific_domains.min' => 'Veuillez sélectionner au moins une discipline scientifique.',
+            'scientific_domains.*.integer' => 'Chaque discipline scientifique sélectionnée doit être valide.',
+            'scientific_domains.*.exists' => 'La discipline scientifique sélectionnée est invalide.',
+            'geo_zones.array' => 'Les zones géographiques doivent être remplies.',
+            'deadlines.array' => 'Les deadlines doivent être remplies.',
+            'short_description.string' => 'La description courte doit être une chaîne de caractères.',
+            'short_description.max' => 'La description courte ne peut pas dépasser :max caractères.',
+            'long_description.array' => 'La description longue doit être remplie.',
+            'funding.array' => 'Le champs "Budget & dépenses" doit être rempli.',
+            'apply_instructions.array' => 'Les instructions pour postuler doivent être remplis.',
+            'contact_ulb.*.first_name.string' => 'Le prénom du contact interne doit être une chaîne de caractères.',
+            'contact_ulb.*.last_name.string' => 'Le nom du contact interne doit être une chaîne de caractères.',
+            'contact_ulb.*.email.email' => 'L\'email du contact interne doit être une adresse email valide.',
+            'contact_ext.*.first_name.string' => 'Le prénom du contact externe doit être une chaîne de caractères.',
+            'contact_ext.*.first_name.max' => 'Le prénom du contact externe ne peut pas dépasser :max caractères.',
+            'contact_ext.*.last_name.string' => 'Le nom du contact externe doit être une chaîne de caractères.',
+            'contact_ext.*.last_name.max' => 'Le nom du contact externe ne peut pas dépasser :max caractères.',
+            'contact_ext.*.email.email' => 'L\'email du contact externe doit être une adresse email valide.',
+            'contact_ext.*.email.max' => 'L\'email du contact externe ne peut pas dépasser :max caractères.',
+            'at_least_one_contact' => 'Veuillez fournir au moins un contact interne ou externe.',
+            'status.integer' => 'Le statut doit être un nombre entier.',
+            'is_draft.boolean' => 'Le champ "Brouillon" doit être vrai ou faux.',
+            'info_sessions.array' => 'Les séances d\'informations doivent être remplies.'
+        ];
+
+        $validator = Validator::make($this->data, $rules, $messages, [
             'title' => 'Titre',
             'is_big' => 'Projet Majeur',
             'organisation_id' => 'Organisation',
             'info_types' => 'Types de programme',
             'scientific_domains' => 'Disciplines scientifiques',
-            'scientific_domains.required' => 'Veuillez sélectionner au moins une discipline scientifique.',
-            'scientific_domains.min' => 'Veuillez sélectionner au moins une discipline scientifique.',
-            'scientific_domains.*.integer' => 'Chaque discipline scientifique sélectionnée doit être valide.',
-            'scientific_domains.*.exists' => 'La discipline scientifique sélectionnée est invalide.',
             'geo_zones' => 'Zones géographiques',
             'deadlines' => 'Deadlines',
+            // 'date_lessor' => 'Date Bailleur',
             'short_description' => 'Description courte',
             'long_description' => 'Description longue',
-            'funding' => 'Financement',
-            'admission_requirements' => 'Requis d\'admission',
-            'apply_instructions' => 'Instructions d\'application',
+            'funding' => 'Budget et dépenses',
+            'admission_requirements' => 'Critères d\'admission',
+            'apply_instructions' => 'Pour postuler',
             'contact_ulb.*.first_name' => 'Prénom',
             'contact_ulb.*.last_name' => 'Nom',
             'contact_ulb.*.email' => 'Email',
             'contact_ext.*.first_name' => 'Prénom',
             'contact_ext.*.last_name' => 'Nom',
             'contact_ext.*.email' => 'Email',
-            'at_least_one_contact' => 'Veuillez fournir au moins un contact interne ou externe.',
             'status' => 'Status',
             'is_draft' => 'Brouillon',
-            'info_sessions' => 'Séances d\'information',
+            'info_sessions' => 'Séance d\'informations'
         ]);
         $validator->after(function ($validator) {
             $contact_ulb = $this->data['contact_ulb'] ?? [];
@@ -480,18 +512,23 @@ class ProjectEditForm extends Component implements HasForms
 
             // Filtrer les contacts vides
             $contact_ulb = array_filter($contact_ulb, function ($contact) {
-                return !empty($contact['first_name']) || !empty($contact['last_name']) || !empty($contact['email']);
+                return !empty(trim($contact['first_name'] ?? ''))
+                    && !empty(trim($contact['last_name'] ?? ''))
+                    && !empty(trim($contact['email'] ?? ''));
             });
 
             $contact_ext = array_filter($contact_ext, function ($contact) {
-                return !empty($contact['first_name']) || !empty($contact['last_name']) || !empty($contact['email']);
+                return !empty(trim($contact['first_name'] ?? ''))
+                    && !empty(trim($contact['last_name'] ?? ''))
+                    && !empty(trim($contact['email'] ?? ''));
             });
 
+            // Vérification si au moins un contact ULB ou externe est fourni
             $ulb_has_contact = !empty($contact_ulb);
             $ext_has_contact = !empty($contact_ext);
 
             if (!$ulb_has_contact && !$ext_has_contact) {
-                $validator->errors()->add('contact_ulb', 'Veuillez fournir au moins un contact interne ou externe.');
+                $validator->errors()->add('contact_ulb', 'Veuillez fournir au moins un contact interne ou externe avec les informations complètes.');
             }
         });
 
@@ -635,6 +672,18 @@ class ProjectEditForm extends Component implements HasForms
 
     public function saveAsDraft()
     {
+        $userId = Auth::id();
+
+        if (!$this->fileService) {
+            $this->fileService = app(FileService::class);
+        }
+
+        if (!empty($this->data['documents'])) {
+            $lastdoc = $this->draft ? $this->draft->content['documents'] : [];
+            $docs = $this->fileService->moveForDraft($this->data['documents'], $lastdoc);
+            $this->data["documents"] = $docs;
+        }
+
         if ($this->draft) {
             $updatedDraft = Draft::find($this->draft->id);
 
