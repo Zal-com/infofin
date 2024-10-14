164f4980af9602072f65d5df9cf7bd399d792382
Guillaume Stordeur <guillaume.stordeur@ulb.be>
2024-10-14 13:03:12 +0200

    feat(Migrations)!: new migrations for info_types refactor

---
+++ app/Models/Expense.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title'];
+    public $timestamps = false;
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'projects_expenses', 'expense_id', 'project_id');
+    }
+}
