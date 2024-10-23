e3fe1f00d0026b777692047550209aadfad2730c
Guillaume Stordeur <guillaume.stordeur@ulb.be>
2024-10-23 14:09:03 +0200

    fix(CollectionEdit): add styling and label to back button + edit url property ???

--- resources/views/livewire/collection-edit.blade.php
+++ resources/views/livewire/collection-edit.blade.php
@@ -1,8 +1,11 @@
 <div>
-    <x-filament::icon-button tag="a" icon="heroicon-o-arrow-uturn-left"
-                             href="{{\Illuminate\Support\Facades\URL::previous()}}"
-    >RetourÒÒ
-    </x-filament::icon-button>
+    <div class="flex flex-col w-fit hover:underline hover:text-blue-500">
+        <x-filament::icon-button tag="a" icon="heroicon-o-arrow-uturn-left"
+                                 href="{{\Illuminate\Support\Facades\URL::previous()}}"
+        />
+        <span>Retour</span>
+    </div>
+
     <h2 class="my-4">Modifier la collection</h2>
     <div class="flex flex-col gap-6">
         {{$this->form}}
