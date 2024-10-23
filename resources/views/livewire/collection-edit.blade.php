e7977322d230993cb52055510aef8660e0059031
Guillaume Stordeur <guillaume.stordeur@ulb.be>
2024-10-23 13:13:18 +0200

    feat(CollectionEdit): back button

--- resources/views/livewire/collection-edit.blade.php
+++ resources/views/livewire/collection-edit.blade.php
@@ -1,4 +1,8 @@
-<div class="flex flex-col gap-6">
-    {{$this->form}}
-    {{$this->table}}
+<div>
+    <x-filament::icon-button tag="a" icon="heroicon-arrow-uturn-left" href="{{back()}}" label="Retour"/>
+    <h2>Modifier la collection</h2>
+    <div class="flex flex-col gap-6">
+        {{$this->form}}
+        {{$this->table}}
+    </div>
 </div>
