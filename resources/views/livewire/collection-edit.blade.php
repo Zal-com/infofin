1528acf5a984bab70be7af565368335bff282052
Guillaume Stordeur <guillaume.stordeur@ulb.be>
2024-10-23 13:22:40 +0200

    fix(CollectionEdit): typo

--- resources/views/livewire/collection-edit.blade.php
+++ resources/views/livewire/collection-edit.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-filament::icon-button tag="a" icon="heroicon-arrow-uturn-left" href="{{back()}}" label="Retour"/>
+    <x-filament::icon-button tag="a" icon="heroicon-o-arrow-uturn-left" href="{{back()}}" label="Retour"/>
     <h2>Modifier la collection</h2>
     <div class="flex flex-col gap-6">
         {{$this->form}}
