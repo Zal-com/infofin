fb5af295b3f168b0d460c2c588020f6d03ad3cef
Guillaume Stordeur <guillaume.stordeur@ulb.be>
2024-10-25 08:36:24 +0200

    fix(CollectionEdit): back button according to claude

--- resources/views/livewire/collection-edit.blade.php
+++ resources/views/livewire/collection-edit.blade.php
@@ -1,8 +1,10 @@
 <div>
-    <x-filament::icon-button icon="heroicon-o-arrow-uturn-left"
-                             href="{{\Illuminate\Support\Facades\URL::previous()}}"
-                             label="Retour"
-    />
+    <x-filament::button
+        icon="heroicon-o-arrow-uturn-left"
+        href="{{\Illuminate\Support\Facades\URL::previous()}}"
+    >
+        Retour
+    </x-filament::button>
     <h2 class="my-4">Modifier la collection</h2>
     <div class="flex flex-col gap-6">
         {{$this->form}}
