b91fb71b3d084d1734664e0f1f7d0e5687667d25
ahof0006 <axel.hoffmann@ulb.be>
2024-10-25 14:55:21 +0200

    feat(agenda): added projet to legend

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -5,6 +5,10 @@
             <h2 class="mb-4">Calendrier de projets</h2>
             <div class="flex flex-row gap-2 items-center">
                 <div class="flex flex-row gap-2 items-center mr-4">
+                    <div class="h-5 w-5 rounded-md bg-blue-500"></div>
+                    Projet
+                </div>
+                <div class="flex flex-row gap-2 items-center mr-4">
                     <div class="h-5 w-5 rounded-md bg-green-500"></div>
                     Séance d'informations
                 </div>
