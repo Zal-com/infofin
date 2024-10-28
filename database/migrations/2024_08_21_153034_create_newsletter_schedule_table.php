45da7320e9d37758aa66c137b2e13be19c6b3651
ahof0006 <axel.hoffmann@ulb.be>
2024-10-28 14:46:31 +0100

    fix(migration): day_of_week tinyint instead of string

--- database/migrations/2024_08_21_153034_create_newsletter_schedule_table.php
+++ database/migrations/2024_08_21_153034_create_newsletter_schedule_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('newsletter_schedule', function (Blueprint $table) {
             $table->id();
-            $table->string('day_of_week'); // Stocker le jour de la semaine (0 = Sunday, 1 = Monday, etc.)
+            $table->tinyInteger('day_of_week', 2); // Stocker le jour de la semaine (0 = Sunday, 1 = Monday, etc.)
             $table->time('send_time');     // Stocker l'heure d'envoi
             $table->boolean('is_active')->default(true);
             $table->string('message')->nullable()->default(null);
