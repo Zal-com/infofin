c315322f7a86ddddd73ab957c1ab329a81853496
ahof0006 <axel.hoffmann@ulb.be>
2024-11-25 14:07:51 +0100

    feat(admin): added activity & expenses resources

---
+++ app/Filament/Resources/ActivityResource/Pages/ListActivities.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\ActivityResource\Pages;
+
+use App\Filament\Resources\ActivityResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListActivities extends ListRecords
+{
+    protected static string $resource = ActivityResource::class;
+
+
+    protected static ?string $title = "Liste des activités";
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
