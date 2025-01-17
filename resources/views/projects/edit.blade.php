9d3590febdfef7f61efb87aa7310308193ac4aaa
Guillaume Stordeur <guillaume.stordeur@ulb.be>
2025-01-17 17:30:17 +0100

    fix(draft): text classes

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     @props(['project'])
     <div class="py-4">
-        <h2>Edition d'un projet</h2>
+        <h2 class="text-2xl font-semibold">Edition d'un projet</h2>
         <div class="mt-5">
             @livewire('project-edit-form', ['project' => $project])
         </div>
