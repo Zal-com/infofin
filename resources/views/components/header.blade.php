c0411cb6ed130cc2df94cf4051f0589ca3fd4aad
gsto0004 <guillaume.stordeur@ulb.be>
2025-08-04 10:40:44 +0200

    SEO improvements

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="flex flex-col md:flex-row justify-between items-center container m-auto">
         <!-- Section gauche -->
         <a href="{{ route('home') }}" class="flex flex-row gap-4 items-center">
-            <img src="{{ asset('img/ulb_logo.png') }}" class="h-14 border-r-2 pr-3 border-blue-900">
+            <img src="{{ asset('img/ulb_logo.png') }}" class="h-14 border-r-2 pr-3 border-blue-900" alt="Logo de l'Université Libre de Bruxelles">
             <h1 class="text-blue-900 text-3xl font-bold">Infofin</h1>
         </a>
 
